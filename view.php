fc8e4f4135c769fd84d6798af9db25b8675e739f
Magali Fabri <magali_90@live.nl>
2022-02-19 10:20:53 +0100

    Change form layout to work better with error messages

--- view.php
+++ view.php
@@ -18,54 +18,64 @@
     </header>
 
     <form action="" method="POST">
-        <!-- FIRST CURRENCY SELECTION -->
-        <select name="currencies1" id="currencies1">
-            <option value="usd" <?= selected("usd", "currencies1") ?>>$</option>
-            <option value="eur" <?= selected("eur", "currencies1") ?>>€</option>
-            <option value="jpy" <?= selected("jpy", "currencies1") ?>>¥</option>
-            <option value="gbp" <?= selected("gbp", "currencies1") ?>>£</option>
-            <option value="chf" <?= selected("chf", "currencies1") ?>>₣</option>
-        </select>
-        <!-- error message -->
+
+        <div class="row">
+
+            <!-- 1ST CURRENCY SELECTION -->
+            <select name="currencies1" id="currencies1">
+                <option value="usd" <?= selected("usd", "currencies1") ?>>$</option>
+                <option value="eur" <?= selected("eur", "currencies1") ?>>€</option>
+                <option value="jpy" <?= selected("jpy", "currencies1") ?>>¥</option>
+                <option value="gbp" <?= selected("gbp", "currencies1") ?>>£</option>
+                <option value="chf" <?= selected("chf", "currencies1") ?>>₣</option>
+            </select>
+
+            <!-- PRICE INPUT -->
+            <input type="number" step="0.01" min="0.01" id="price" name="price" value="<?= $_POST["price"] ?? "" ?>" placeholder="0" required autocomplete="off">
+            <!-- <input type="text" step="0.01" id="price" name="price" value="<?= $_POST["price"] ?? "" ?>" placeholder="0"> -->
+
+        </div>
+
+        <!-- 1st currency selection error message -->
         <?php if (!empty($validationErrors["currencies1"])) : ?>
-            <p class="error-message"><?= $validationErrors["currencies1"] ?? "" ?></p>
+            <span class="error-message"><?= $validationErrors["currencies1"] ?? "" ?></span>
         <?php endif ?>
 
-        <!-- PRICE INPUT -->
-        <!-- <input type="number" step="0.01" min="0" id="price" name="price" value="<?= $_POST["price"] ?? "" ?>" placeholder="0" required> -->
-        <input type="text" step="0.01" id="price" name="price" value="<?= $_POST["price"] ?? "" ?>" placeholder="0">
-        <!-- error message -->
+        <!-- price input error message -->
         <?php if (!empty($validationErrors["price"])) : ?>
-            <p class="error-message"><?= $validationErrors["price"] ?? "" ?></p>
+            <span class="error-message"><?= $validationErrors["price"] ?? "" ?></span>
         <?php endif ?>
 
-        <br>
-        <br>
+        <div class="row">
 
-        <span>=</span>
+            <span>=</span>
 
-        <!-- SECOND CURRENCY SELECTION -->
-        <select name="currencies2" id="currencies2">
-            <option value="usd" <?= selected("usd", "currencies2") ?>>$</option>
-            <option value="eur" <?= selected("eur", "currencies2") ?>>€</option>
-            <option value="jpy" <?= selected("jpy", "currencies2") ?>>¥</option>
-            <option value="gbp" <?= selected("gbp", "currencies2") ?>>£</option>
-            <option value="chf" <?= selected("chf", "currencies2") ?>>₣</option>
-        </select>
-        <!-- error message -->
+            <!-- 2ND CURRENCY SELECTION -->
+            <select name="currencies2" id="currencies2">
+                <option value="usd" <?= selected("usd", "currencies2") ?>>$</option>
+                <option value="eur" <?= selected("eur", "currencies2") ?>>€</option>
+                <option value="jpy" <?= selected("jpy", "currencies2") ?>>¥</option>
+                <option value="gbp" <?= selected("gbp", "currencies2") ?>>£</option>
+                <option value="chf" <?= selected("chf", "currencies2") ?>>₣</option>
+            </select>
+
+            <!-- CONVERTED PRICE OUTPUT -->
+            <?php if (!empty($_POST["submit"]) && !empty($convertedPrice)) : ?>
+                <span class="converted-price"><?= $convertedPrice ?></span>
+            <?php endif ?>
+
+        </div>
+
+        <!-- 2nd currency selection error message -->
         <?php if (!empty($validationErrors["currencies2"])) : ?>
-            <p class="error-message"><?= $validationErrors["currencies2"] ?? "" ?></p>
+            <span class="error-message"><?= $validationErrors["currencies2"] ?? "" ?></span>
         <?php endif ?>
-
-        <!-- CONVERTED PRICE OUTPUT -->
-        <?php if (!empty($_POST["submit"]) && !empty($convertedPrice)) : ?>
-            <span class="converted-price"><?= $convertedPrice ?></span>
-        <?php endif ?>
-        <br>
 
         <!-- BUTTONS -->
-        <button class="button" type="submit" name="submit" value="convert">convert</button>
-        <button class="button" type="submit" name="submit" value="swap">↑ swap ↓</button>
+        <div class="row">
+            <button class="button" type="submit" name="submit" value="convert">convert</button>
+            <button class="button" type="submit" name="submit" value="swap">↑ swap ↓</button>
+        </div>
 
     </form>
 
